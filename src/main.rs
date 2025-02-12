720adf68137060fe623899d32c951632fd22f07b
decay <decayhonor@gmail.com>
2025-02-12 20:12:18 +0300

    some progress on ch3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    atomics_n_locks::ch1_basic_concurrency::run();
-    atomics_n_locks::ch2_atomics::run();
+    // atomics_n_locks::ch1_basic_concurrency::run();
+    // atomics_n_locks::ch2_atomics::run();
+    atomics_n_locks::ch3_memory_ordering::run();
 }
