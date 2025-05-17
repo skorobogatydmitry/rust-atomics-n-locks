758d6e8e5b2b5af06f83fb99729a6467c91e65e7
decay <decayhonor@gmail.com>
2025-05-17 15:12:43 +0400

    a little progress

--- src/ch6_building_our_own_arc/basic.rs
+++ src/ch6_building_our_own_arc/basic.rs
@@ -30,7 +30,7 @@ unsafe impl<Y: Send + Sync> Sync for Arc<Y> {}
 impl<Y> Arc<Y> {
     /*
      * Make allocation using Box, then leak it and store
-     * it guarantees data validity, as the allocation hides from Rust's radars
+     * It guarantees data validity, as the allocation hides from Rust's radars
      */
     pub fn new(data: Y) -> Self {
         Self {
@@ -77,7 +77,8 @@ impl<Y> Drop for Arc<Y> {
     fn drop(&mut self) {
         /*
          * Relaxed ordering doesn't work, as another thread may be clonning or dropping the Arc.
-         * All but last drops should happens-before the last one => Release for all subs and a fence for the rest
+         * All but last drops should happens-before the last one =>
+         *  Release for all other subs and a fence to wait for the main drop
          */
         if self.data().ref_cnt.fetch_sub(1, Release) == 1 {
             fence(Acquire);
@@ -93,7 +94,7 @@ mod test {
     use super::*;
     use std::{sync::atomic::AtomicUsize, thread::spawn};
 
-    // a special drops counter to be enveloed to Arc
+    // a special drops counter to be enveloped to Arc
     static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
     struct DetectDrop;
     impl Drop for DetectDrop {
@@ -123,5 +124,26 @@ mod test {
         // now drop Y
         drop(y);
         assert_eq!(1, NUM_DROPS.load(Relaxed));
+    }
+}
+
+/// # Mutation
+/// A simple case - allow accessing data only when ref counter is 1  
+/// get_mut also should only ensure exclusiveness => &mut self as an input
+
+impl<Y> Arc<Y> {
+    // the parameter isn't `&mut self', to allow the underlying Y to be allowed to have get_mut of his own called
+    pub fn get_mut(the_arc: &mut Self) -> Option<&mut Y> {
+        // Relaxed works just fine, as we don't care about the actual moment
+        // and nothing could "steal" our Arc anymore, as it's &mut
+        if the_arc.data().ref_cnt.load(Relaxed) == 1 {
+            // make sure all the drops are actually finished
+            fence(Acquire);
+            // SAFETY: Nothing else can access the data, since there's only 1 Arc
+            // and we have exclusive access to it.
+            unsafe { Some(&mut the_arc.ptr.as_mut().data) }
+        } else {
+            None
+        }
     }
 }
