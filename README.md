300d537b9488d4de47b435a4986e22ea9d9843ec
decay <decayhonor@gmail.com>
2025-06-13 11:03:22 +0400

    minor improvements & mention docs

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Author: https://x.com/m_ou_se  
 E-book: https://marabos.nl/atomics/
+
+It's useful to open the crate as a doc: `cargo doc --open --document-private-items`. Although it doesn't show most of the notes as documentation => better to open sources in the docs.
--- src/ch2_atomics/mod.rs
+++ src/ch2_atomics/mod.rs
@@ -2,11 +2,12 @@ mod atomic_load_and_store_operations;
 mod compare_and_exchange_operations;
 mod fetch_and_modify_operations;
 
+/**
+ * atomic types live in std::sync::atomic
+ * they're are all internally mutable
+ * all atomics have memory ordering arg, it'll be reviewed in chapter 3
+ */
 pub fn run() {
-    // atomic types live in std::sync::atomic
-    // they're are all internally mutable
-    // all atomics have memory ordering arg, it'll be reviewed in chapter 3
-
     atomic_load_and_store_operations::run();
     fetch_and_modify_operations::run();
     compare_and_exchange_operations::run();
